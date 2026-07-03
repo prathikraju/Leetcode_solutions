1f7af36a529e62a7481e443a912df0e6efd40426
prathikraju <166566839+prathikraju@users.noreply.github.com>
2026-07-02 19:33:21 -0500

    Time: 383 ms (28.95%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,14 +1,11 @@
 # Write your MySQL query statement below
 SELECT 
-    S1.name
-FROM 
-    (SELECT 
-    E1.id, E1.name, COUNT(*) AS NUMBER
+    E1.name
 FROM
     Employee E1
 JOIN
     Employee E2 ON E1.id = E2.managerId 
 GROUP BY 
-    E1.id, E1.name) AS S1
-WHERE 
-    S1.NUMBER >= 5;
+    E1.id, E1.name
+HAVING
+   COUNT(*) >= 5;
