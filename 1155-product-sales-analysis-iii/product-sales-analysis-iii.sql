81886f6f354eef43427ffc204c745723b826f690
prathikraju <166566839+prathikraju@users.noreply.github.com>
2026-07-06 16:03:57 -0500

    Time: 711 ms (66.02%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,18 +1,5 @@
 # Write your MySQL query statement below
 /*SELECT 
-    product_id, year AS first_year, quantity, price
-FROM 
-    C
-WHERE 
-    (product_id, year) IN(
-SELECT
-    product_id, MIN(year)
-FROM 
-    Sales
-GROUP BY 
-    product_id);*/
-
-SELECT 
     S1.product_id, S1.year AS first_year, S1.quantity, S1.price   
 FROM 
     Sales S1
@@ -26,4 +13,19 @@ GROUP BY
 ON 
     S1.product_id = S2.product_id
 AND 
-    year = minimum_year
+    year = minimum_year*/
+
+SELECT 
+    product_id, year AS first_year, quantity, price
+FROM 
+    Sales
+WHERE 
+    (product_id, year) IN(
+SELECT
+    product_id, MIN(year)
+FROM 
+    Sales
+GROUP BY 
+    product_id);
+
+
